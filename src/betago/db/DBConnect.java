afe2f6f4317d569ca5d1f0cffcf18f6b73a3bf1a
Xinyu-Zhang9943 <47932699+Xinyu-Zhang9943@users.noreply.github.com>
2020-05-07 16:14:22 +0800

    Create DBConnect.java

---
+++ src/betago/db/DBConnect.java
@@ -0,0 +1,34 @@
+package betago.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+	private final String DBDRIVER = "com.mysql.cj.jdbc.Driver";   
+    private final String DBURL = "jdbc:mysql://localhost:3306/natum?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";   
+    private final String DBUSER = "root";   
+    private final String DBPASSWORD = "123456";   
+    private Connection conn = null ;   
+  
+    public DBConnect()   {   
+        try{   
+            Class.forName(DBDRIVER);   
+            this.conn = DriverManager.getConnection(DBURL,DBUSER,DBPASSWORD) ;  
+        }catch (Exception e){ 
+        	System.out.println(e.getMessage());  
+        	}   
+        
+    }   
+    
+    // 取得数据库连接   
+    public Connection getConnection(){ 
+        return this.conn ;   
+    }   
+  
+    // 关闭数据库连接   
+    public void close(){   
+        try{   
+            this.conn.close() ;   
+        }catch (Exception e){ }          
+    }
+}
